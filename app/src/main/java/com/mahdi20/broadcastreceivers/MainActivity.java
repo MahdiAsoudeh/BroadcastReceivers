875df9c1981410569f17175c575823bd5631f190
mahdi asoodeh <asoodeh.dev@gmail.com>
2020-02-18 23:40:52 +0330

    added internet change listener

--- app/src/main/java/com/mahdi20/broadcastreceivers/MainActivity.java
+++ app/src/main/java/com/mahdi20/broadcastreceivers/MainActivity.java
@@ -2,13 +2,81 @@ package com.mahdi20.broadcastreceivers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private BroadcastReceiver mNetworkReceiver;
+    static TextView tv_check_connection;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tv_check_connection = (TextView) findViewById(R.id.tv_check_connection);
+        mNetworkReceiver = new NetworkChangeReceiver();
+        registerNetworkBroadcastForNougat();
+
+
+    }
+
+
+    public static void dialog(boolean value) {
+
+        if (value) {
+            tv_check_connection.setText("We are back !!!");
+            tv_check_connection.setBackgroundColor(Color.GREEN);
+            tv_check_connection.setTextColor(Color.WHITE);
+
+            Handler handler = new Handler();
+            Runnable delayrunnable = new Runnable() {
+                @Override
+                public void run() {
+                    tv_check_connection.setVisibility(View.GONE);
+                }
+            };
+            handler.postDelayed(delayrunnable, 3000);
+        } else {
+            tv_check_connection.setVisibility(View.VISIBLE);
+            tv_check_connection.setText("Could not Connect to internet");
+            tv_check_connection.setBackgroundColor(Color.RED);
+            tv_check_connection.setTextColor(Color.WHITE);
+        }
+    }
+
+
+    private void registerNetworkBroadcastForNougat() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            registerReceiver(mNetworkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            registerReceiver(mNetworkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
+    }
+
+    protected void unregisterNetworkChanges() {
+        try {
+            unregisterReceiver(mNetworkReceiver);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterNetworkChanges();
     }
 }
